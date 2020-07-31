90bc7de0af7f795553b6ce6f0c13a69c399c1ff0
tosihisamitunaga <68527250+tosihisamitunaga@users.noreply.github.com>
2020-07-31 16:35:58 +0900

    Revert "レスポンスの設定"
    This reverts commit 9b254a7afa2c0619b2a4a6fe532a2dfb4d105359.

--- app/assets/config/routes.rb
+++ app/assets/config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  root to: 'posts#index'
-  post 'posts', to: 'posts#create'
-  get 'posts/:id', to: 'posts#checked'
-  
+  get 'posts', to: 'posts#index'
+  get 'posts/new', to: 'posts#new'
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,13 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all.order(id: "DESC") # 1番目のレコードを@postに代入
+    @posts = Post.all  # 1番目のレコードを@postに代入
   end
 
-  def create
-    Post.create(content: params[:content])
-    redirect_to action: :index
-  end
 
-  def checked
-    post = Post.find(params[:id])
-    if post.checked then
-      post.update(checked: false)
-    else
-      post.update(checked: true)
-    end
+ def new
+ end
 
-    item = Post.find(params[:id])
-    render json: { post: item }
-  end
+ def create
+   Post.create(content: params[:content])
+ end
 end
